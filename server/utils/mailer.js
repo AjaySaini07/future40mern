b1f623506ffef7bab9bcb32b266ddd68e7bd8641
Ajay <sain95098@gmail.com>
2026-03-05 12:15:58 +0530

    Signup issue fixing on live

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,13 +1,31 @@
+// const nodemailer = require("nodemailer");
+
+// // Gmail transporter
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.GMAIL_USER,
+//     pass: process.env.GMAIL_APP_PASSWORD,
+//   },
+// });
+
+// module.exports = transporter;
+
 const nodemailer = require("nodemailer");
 
-// Gmail transporter
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
+  },
+  connectionTimeout: 20000,
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
